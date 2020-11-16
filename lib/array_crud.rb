e199f4876860fe7c81ed4c9c22c2e4eb2b9bab3a
Christopher <christopher.petronella@gmail.com>
2020-11-15 19:00:38 -0500

    commit tree 4cad0aaf9d2694d43fe21bbe4fa5fd7b6628a078

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,45 @@
 def create_an_empty_array
-  
+    array_empty = []
 end
 
 def create_an_array
-  
+    create_an_array = ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+    add_element_to_end_of_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    add_element_to_start_of_array = ["I", "am", "really", "learning"]
+    add_element_to_start_of_array.unshift("wow")
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array = ["I", "am", "really", "learning", "arrays!"]
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array [2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array [5]
 end
+
+
